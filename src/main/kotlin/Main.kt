a6d56708c31f92b876d3c87bc25dc483e0095a6f
Shrikant Govande <govandeshrikant6@gmail.com>
2022-10-05 09:19:32 +0530

    Kotlin Basic upto Collection

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,15 @@
+
+
 fun main(args: Array<String>) {
-    BasicFunctions().showOrder()
+//    val s="bitcoin take over the world maybe who knows perhaps"
+//    val lstValues: List<String> = s.split(" ").map { it -> it.trim() }
+//    var lengthword= listOf<Int>()
+//    for (word in lstValues){
+//        lengthword+=word.length
+//    }
+//    lengthword.minOrNull()?.let { println(it.toInt()) }
+
+    BasicCollection().printList()
 
 }
 
